ade88d4007256354ff796ce8f0d9de3c524d366c
Abderraouf Zine <rofazayn@gmail.com>
2019-12-14 15:05:46 +0000

    Create routes and views redirect as fallback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Wrapper from './components/Wrapper/Wrapper';
-import Navbar from './components/Navbar/Navbar';
 import Container from './components/Container/Container';
+import Navbar from './components/Navbar/Navbar';
+import Wrapper from './components/Wrapper/Wrapper';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <Wrapper>
       <Container>
         <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Redirect to='/' />
+        </Switch>
       </Container>
-      {/* <h1>Hello world</h1> */}
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import theme from './assets/theming/theme';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import App from './App';
+import theme from './assets/theming/theme';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 const root = (
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <Router>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Router>
 );
 
 const manifest = document.getElementById('root');
