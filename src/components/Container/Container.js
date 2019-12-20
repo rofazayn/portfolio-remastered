9670525ae9715cb6fe793ff89013acdf01a04eb1
Abderraouf Zine <rofazayn@gmail.com>
2019-12-20 12:46:17 +0000

    Made the container responsive

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,11 +3,28 @@ import styled from 'styled-components';
 
 const SContainer = styled.div`
   width: 100%;
-  max-width: 1440px;
+  max-width: 1200px;
   margin: 0 auto;
   padding: 0 2rem;
-  @media (max-width: 1440px) {
-    max-width: 1240px;
+  // Large devices (desktops, less than 1200px)
+  // Small devices (landscape phones, 576px and up)
+  @media (min-width: 576px) {
+    max-width: 400px;
+  }
+
+  // Medium devices (tablets, 768px and up)
+  @media (min-width: 768px) {
+    max-width: 668px;
+  }
+
+  // Large devices (desktops, 992px and up)
+  @media (min-width: 992px) {
+    max-width: 898px;
+  }
+
+  // Extra large devices (large desktops, 1200px and up)
+  @media (min-width: 1200px) {
+    max-width: 1000px;
   }
 `;
 
