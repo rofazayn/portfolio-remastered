e54e83f5b26038ee1c70cc8977e3600d5cc6d2e0
Abderraouf Zine <rofazayn@gmail.com>
2019-12-21 19:18:06 +0000

    Switched back to default router instead of HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -9,12 +9,12 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 const root = (
-  <HashRouter basename='/'>
+  <Router basename='/'>
     <Provider store={store}>
       <ScrollToTop />
       <App />
     </Provider>
-  </HashRouter>
+  </Router>
 );
 
 const manifest = document.getElementById('root');
