271eedab6288346569a7d1943b7fec2d50b35332
Abderraouf Zine <rofazayn@gmail.com>
2020-01-11 22:56:04 +0000

    Hid all scrollbars.

--- src/helpers/GlobalStyles.js
+++ src/helpers/GlobalStyles.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  ::-webkit-scrollbar {
+      display: none;
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
