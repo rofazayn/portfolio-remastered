80d8d9482b978695a7c262cf24fb4eb92e2add2f
Abderraouf Zine <rofazayn@gmail.com>
2020-01-16 10:20:49 +0000

    Changed skill levels terminology, a big thank you to Karl Hadwen for this tip.

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -128,34 +128,34 @@ const Resume = ({ pageTitle }) => {
                   <li className='item'>
                     <img src={htmlImage} alt='HTML' />
                     <h4 className='item__title'>HTML</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Expert</p>
                   </li>
                   <li className='item'>
                     <img src={cssImage} alt='CSS' />
                     <h4 className='item__title'>CSS</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Expert</p>
                   </li>
                   <li className='item'>
                     <img src={javascriptImage} alt='JavaScript' />
                     <h4 className='item__title'>JavaScript</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                 </ul>
                 <ul>
                   <li className='item'>
                     <img src={reactImage} alt='ReactJS' />
                     <h4 className='item__title'>React JS</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                   <li className='item'>
                     <img src={sassImage} alt='Sass' />
                     <h4 className='item__title'>Sass</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Expert</p>
                   </li>
                   <li className='item'>
                     <img src={gatsbyImage} alt='Gatsby' />
                     <h4 className='item__title'>Gatsby</h4>
-                    <p className='item__level'>Good</p>
+                    <p className='item__level'>Competent</p>
                   </li>
                 </ul>
               </div>
@@ -165,17 +165,17 @@ const Resume = ({ pageTitle }) => {
                   <li className='item'>
                     <img src={nodejsImage} alt='NodeJS' />
                     <h4 className='item__title'>Node JS</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                   <li className='item'>
                     <img src={expressImage} alt='Express JS' />
                     <h4 className='item__title'>Express JS</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                   <li className='item'>
                     <img src={mongodbImage} alt='MongoDB' />
                     <h4 className='item__title'>MongoDB</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Competent</p>
                   </li>
                 </ul>
               </div>
@@ -185,34 +185,34 @@ const Resume = ({ pageTitle }) => {
                   <li className='item'>
                     <img src={vscodeImage} alt='VSCode' />
                     <h4 className='item__title'>VSCode</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Expert</p>
                   </li>
                   <li className='item'>
                     <img src={gitImage} alt='Git' />
                     <h4 className='item__title'>Git</h4>
-                    <p className='item__level'>Good</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                   <li className='item'>
                     <img src={postmanImage} alt='Postman' />
                     <h4 className='item__title'>Postman</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                 </ul>
                 <ul>
                   <li className='item'>
                     <img src={adobexdImage} alt='Adobe XD' />
                     <h4 className='item__title'>Adobe XD</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                   <li className='item'>
                     <img src={adobeIllustratorImage} alt='Adobe AI' />
                     <h4 className='item__title'>Adobe AI</h4>
-                    <p className='item__level'>Good</p>
+                    <p className='item__level'>Competent</p>
                   </li>
                   <li className='item'>
                     <img src={githubImage} alt='GitHub' />
                     <h4 className='item__title'>GitHub</h4>
-                    <p className='item__level'>Good</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                 </ul>
               </div>
@@ -222,51 +222,51 @@ const Resume = ({ pageTitle }) => {
                   <li className='item'>
                     <img src={graphqlImage} alt='GraphQL' />
                     <h4 className='item__title'>GraphQL</h4>
-                    <p className='item__level'>Basic</p>
+                    <p className='item__level'>Beginner</p>
                   </li>
                   <li className='item'>
                     <img src={apolloImage} alt='Apollo JS' />
                     <h4 className='item__title'>Apollo JS</h4>
-                    <p className='item__level'>Basic</p>
+                    <p className='item__level'>Beginner</p>
                   </li>
                   <li className='item'>
                     <img src={socketioImage} alt='Socket IO' />
                     <h4 className='item__title'>Socket IO</h4>
-                    <p className='item__level'>Basic</p>
+                    <p className='item__level'>Beginner</p>
                   </li>
                 </ul>
                 <ul>
                   <li className='item'>
                     <img src={jestImage} alt='Jest JS' />
                     <h4 className='item__title'>Jest JS</h4>
-                    <p className='item__level'>Basic</p>
+                    <p className='item__level'>Beginner</p>
                   </li>
                   <li className='item'>
                     <img src={reduxImage} alt='Redux' />
                     <h4 className='item__title'>Redux</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                   <li className='item'>
                     <img src={firebaseImage} alt='Firebase' />
                     <h4 className='item__title'>Firebase</h4>
-                    <p className='item__level'>Good</p>
+                    <p className='item__level'>Competent</p>
                   </li>
                 </ul>
                 <ul>
                   <li className='item'>
                     <img src={webpackImage} alt='Webpack' />
                     <h4 className='item__title'>Webpack</h4>
-                    <p className='item__level'>Basic</p>
+                    <p className='item__level'>Beginner</p>
                   </li>
                   <li className='item'>
                     <img src={scomponentsImage} alt='SComponents' />
                     <h4 className='item__title'>SComponents</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Expert</p>
                   </li>
                   <li className='item'>
                     <img src={npmImage} alt='NPM' />
                     <h4 className='item__title'>NPM</h4>
-                    <p className='item__level'>Great</p>
+                    <p className='item__level'>Proficient</p>
                   </li>
                 </ul>
               </div>
